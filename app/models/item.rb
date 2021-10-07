85495e4de8a1c8dc87187fa223cddbe95fd75021
kobakoba1 <kobayashihiroshi@kobayashihiroshinoMacBook-Pro.local>
2021-10-07 15:13:50 +0900

    バリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,17 @@ class Item < ApplicationRecord
   belongs_to :ship_form
   belongs_to :until_ship_day
   # has_one_attached :image
-  # validates :price, presence: true
-  # validates :title, presence: true
-  # validates :concept, presence: true
+
+  validates :image, presence: true
+  validates :title, presence: true
+  validates :concept, presence: true
+  validates :category, presence: true
+  validates :item_condition, presence: true
+  validates :delivery_fee, presence: true
+  validates :ship_form, presence: true
+  validates :until_ship_day, presence: true
+  validates :price, presence: true
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
   
   # belongs_to :user
 end
