8e5d273190497db447916b02ee85ae1bc6d3d230
kobakoba1 <kobayashihiroshi@kobayashihiroshinoMacBook-Pro.local>
2021-10-16 03:34:39 +0900

    Java

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,6 +4,8 @@ window.addEventListener('load', () => {
     const inputValue = priceInput.value;
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(priceInput.value * 0.1 );
+    const profitNumber = document.getElementById("profit")
+    const value_result = inputValue * 0.1
+    profitNumber.innerHTML = (Math.floor(inputValue - value_result));
   })
-
 });
