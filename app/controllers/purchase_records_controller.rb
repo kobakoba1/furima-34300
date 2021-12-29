24ab83a8af9fa6c9647b20599065cb71b16cba9d
kobakoba1 <kobayashihiroshi@kobayashihiroshinoMacBook-Pro.local>
2021-12-29 13:29:31 +0900

    テストコード

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -2,6 +2,7 @@ class PurchaseRecordsController < ApplicationController
 
   def index
     @purchase_form = PurchaseForm.new
+    @item = Item.find(params[:item_id])
   end 
 
   def create
@@ -12,12 +13,12 @@ class PurchaseRecordsController < ApplicationController
     else
       render :index
     end
-  end  
+  end    
 
   private
 
   def purchase_record_params
     params.require(:purchase_form).permit(:postal_code, :ship_form_id, :city, :house_number, :building_name, 
       :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
-  end  
+  end
 end
--- spec/factories/purchase_forms.rb
+++ spec/factories/purchase_forms.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :purchase_form do
     postal_code { '123-4567' }
-    ship_form_id { 1 }
+    ship_form_id { 2 }
     city { '東京都' }
     house_number { '1-1' }
     building_name { '東京ハイツ' }
-    phone_number { 11111111111 }
+    phone_number { '09011112222' }
   end
 end
